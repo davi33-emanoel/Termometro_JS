47128934d1695bf002da0f1db406328b76617491
davi <emanoeldavi348@gmail.com>
2021-08-04 22:23:27 -0300

    Exercicio de JS

---
+++ termometro.js
@@ -0,0 +1,13 @@
+
+var temperatura = 37
+
+if(temperatura > 37){
+    console.log('Voce esta com febre');
+}
+if(temperatura < 35){
+    console.log('Voce esta com hipotermia');
+}
+else{
+    console.log('nada de febre');
+}
+
